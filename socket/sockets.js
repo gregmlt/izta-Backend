390c7b64285c86d62db774e38db23df278f2590c
GregoireMeillet <meillet.gregoire@gmail.com>
2024-05-28 14:07:53 +0200

    V1 searchBar

---
+++ socket/sockets.js
@@ -0,0 +1,23 @@
+const Company = require("../models/companies");
+
+const sockets = async (io, socket) => {
+  socket.on("searchQuery", async (data) => {
+    const regex = /^\d+$/;
+    let response;
+
+    if (regex.test(data.query)) {
+      const regex = /^\d{9}$/;
+      if (regex.test(data.query)) {
+        response = await Company.find({ siren: data.query });
+      } else {
+        response = await Company.find({ siret: data.query });
+      }
+    } else {
+      response = await Company.find({ companyName: data.query });
+    }
+
+    socket.emit("searchResults", { companies: response });
+  });
+};
+
+module.exports = sockets;
